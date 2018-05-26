1ef93dd0ebcdb604d85fd7d0d2096d85930f9a59
danieljhegeman <student@hackreactor.com>
2018-05-26 16:28:31 -0700

    able to get data from movie API

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var promise = require('promise');
 var app= express();
+var config= require('../config.js')
+var request = require('request')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': false}))
 //helper function to get Movies from API
 var getMovies = function (movieName) {
+  var options ={
+    url:`https://api.themoviedb.org/3/search/movie?api_key=${config.TOKEN}&language=en-US&query=${movieName}&page=1&include_adult=false`,
+    headers: {
+      'User-Agent': 'request',
+    }
+  }
+  
+  return new Promise(function(resolve, reject) {
+    request(options, function(err, res) {
+      if (err) reject (err);
+      else resolve (res);
+    })
+  })
+}
+
+var save = function() {
   
 }
 
@@ -17,7 +36,12 @@ app.post('/movies', function(req, res) {
   //use a helper function to get data from movie API
     //res.send() the data
   getMovies(movieName)
-  res.send(console.log(req.body.value))
+  .then((data) => {
+    res.send(JSON.parse(data.body))
+  })
+  .catch((error) => {
+    console.log('err in catch for post', error)
+  })
 })
 
 app.get('/movies', function(req,res) {
